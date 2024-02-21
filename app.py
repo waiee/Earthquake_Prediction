22519f5dd6f584802df2fb6d0df1871097c0e6ff
Waiee Zainol <69743222+waiee@users.noreply.github.com>
2024-02-22 00:11:28 +0800

    c

--- app.py
+++ app.py
@@ -30,25 +30,11 @@ def home():
 def method():
     st.subheader("Hi, I am Waiee :wave:")
 
-def datanalysis():
+def dataanalysis():
     st.subheader("Hi, I am Waiee :wave:")
 
 def about():
-    dp_image = Image.open("image/removebgWaiee.png")
-    image_column, right_column = st.columns((1,2))
-    with image_column:
-        st.image(dp_image, caption="")
-    with right_column:
-        st.subheader("Hi, I am Waiee :wave:")
-        st.title("Bachelor of Computer Science (Hons.) Data Science")
-        st.write("I am passionate in Data Science, AI and Machine Learning.")
-
-        # Define the GitHub and LinkedIn links
-        github_link = "[GitHub](https://github.com/waiee)"
-        linkedin_link = "[LinkedIn](https://www.linkedin.com/in/waiee-zainol-9b00461ab/)"
-        
-        # Display the sentence with the links
-        st.markdown(f"Check out my {github_link} & {linkedin_link} for more info!")
+    st.subheader("Hi, I am Waiee :wave:")
 
 
 # ----- SIDEBAR ----- #
